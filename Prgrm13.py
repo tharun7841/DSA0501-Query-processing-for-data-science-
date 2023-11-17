c7ea0e3f39bbfb0ad570eeb8b8d315943b5742f5
tharun7841 <112371398+tharun7841@users.noreply.github.com>
2023-11-17 12:33:21 +0530

    Create Prgrm13.py

---
+++ Prgrm13.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data = {'A': [1, 2, None, 4, 5],
+        'B': [None, 2, 3, None, 5],
+        'C': [1, 2, 3, 4, 5]}
+df = pd.DataFrame(data)
+missing_values = df.isna()
+print(missing_values)
